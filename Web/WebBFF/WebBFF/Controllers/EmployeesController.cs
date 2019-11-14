246a361397deb66c57af5a553cb4333d3189ca3b
Andrei Pacurariu <pacurariu_andrei@yahoo.com>
2019-11-14 21:25:59 +0100

    Remove redundant usings...

--- Web/WebBFF/WebBFF/Controllers/EmployeesController.cs
+++ Web/WebBFF/WebBFF/Controllers/EmployeesController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using WebBFF.Contract.Queries;
 
 namespace WebBFF.Controllers
